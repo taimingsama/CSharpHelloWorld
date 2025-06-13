4e1c1e6f96650c8d4cd44ae988ffdf78aaa94c05
Tai Ming <taimingsama@gmail.com>
2025-06-13 12:24:21 +0800

    添加打印一个学生信息

--- CSharpHelloWorld/MainProgram.cs
+++ CSharpHelloWorld/MainProgram.cs
@@ -5,5 +5,9 @@ public class MainProgram
     public static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        
+        var student = new StudentInfo(1, "汰铭");
+
+        Console.WriteLine(student);
     }
 }
